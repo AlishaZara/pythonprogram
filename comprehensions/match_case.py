253888dc33f9fc5e457abddbf2ca9ebf865cde56
Alisha <psalisha03@gmail.com>
2023-12-28 16:27:31 +0530

    Day9b

---
+++ comprehensions/match_case.py
@@ -0,0 +1,17 @@
+seats=int(input('enter the seats...'))
+option=int(input('enter option here...'))
+match option:
+    case 1:
+        print("diamond class")
+        amount=seats*200
+    case 2:
+        print("golden class")
+        amount=seats*150
+    case 3:
+        print("silver class")
+        amount=seats*100
+    case _:
+        print('invalid option')
+        amount=None
+print(amount)
+    
